68795672c1761973088cd467652b0f71ca0f6490
ginabethrussell <ginabeth.russell@gmail.com>
2021-07-21 11:56:38 -0400

    modifies scrollview on welcome screen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,79 +1,94 @@
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import Unorderedlist from "react-native-unordered-list";
 import { Ionicons } from "@expo/vector-icons";
 
 const WelcomeScreen = ({ navigation }) => (
   <View style={styles.screenwrapper}>
-    <Text style={styles.title}>Welcome to Awesome React Native</Text>
-    <Text style={styles.listHeader}>Explore this section to learn: </Text>
-    <View style={styles.optionswrapper}>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <Text style={styles.title}>Welcome to Awesome React Native</Text>
+      <Text style={styles.listHeader}>Explore this section to learn: </Text>
+      <View style={styles.optionswrapper}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("What is React Native")}
+        >
+          <Unorderedlist style={styles.bulletItem}>
+            <Text style={styles.bulletText}>What is React Native?</Text>
+          </Unorderedlist>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("How It Works")}>
+          <Unorderedlist style={styles.bulletItem}>
+            <Text style={styles.bulletText}>How it Works</Text>
+          </Unorderedlist>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Meet the Competition")}
+        >
+          <Unorderedlist style={styles.bulletItem}>
+            <Text style={styles.bulletText}>Its Competition</Text>
+          </Unorderedlist>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Getting Started")}
+        >
+          <Unorderedlist style={styles.bulletItem}>
+            <Text style={styles.bulletText}>
+              and How to Build Your First App
+            </Text>
+          </Unorderedlist>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.listHeader}>Already have some knowledge?</Text>
+      <Text style={styles.listSubHeader}>
+        {" "}
+        Check out the tabs to learn more about:{" "}
+      </Text>
+      <View style={styles.optionswrapper}>
+        <TouchableOpacity onPress={() => navigation.navigate("Navigation")}>
+          <Unorderedlist style={styles.bulletItem}>
+            <Text style={styles.bulletText}>Navigation</Text>
+          </Unorderedlist>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("State Management")}
+        >
+          <Unorderedlist style={styles.bulletItem}>
+            <Text style={styles.bulletText}>State Management</Text>
+          </Unorderedlist>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Native Device Features")}
+        >
+          <Unorderedlist style={styles.bulletItem}>
+            <Text style={styles.bulletText}>Using Native Device Features</Text>
+          </Unorderedlist>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Debugging")}>
+          <Unorderedlist style={styles.bulletItem}>
+            <Text style={styles.bulletText}>Debugging</Text>
+          </Unorderedlist>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Publishing")}>
+          <Unorderedlist style={styles.bulletItem}>
+            <Text style={styles.bulletText}>and Publishing Your App</Text>
+          </Unorderedlist>
+        </TouchableOpacity>
+      </View>
+
       <TouchableOpacity
+        style={styles.nextButton}
         onPress={() => navigation.navigate("What is React Native")}
       >
-        <Unorderedlist style={styles.bulletItem}>
-          <Text style={styles.bulletText}>What is React Native?</Text>
-        </Unorderedlist>
+        <Text style={styles.nextButtonText}>Next</Text>
+        <Ionicons name="arrow-forward-outline" size={24} color="white" />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("How It Works")}>
-        <Unorderedlist style={styles.bulletItem}>
-          <Text style={styles.bulletText}>How it Works</Text>
-        </Unorderedlist>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Meet the Competition")}
-      >
-        <Unorderedlist style={styles.bulletItem}>
-          <Text style={styles.bulletText}>Its Competition</Text>
-        </Unorderedlist>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Getting Started")}>
-        <Unorderedlist style={styles.bulletItem}>
-          <Text style={styles.bulletText}>and How to Build Your First App</Text>
-        </Unorderedlist>
-      </TouchableOpacity>
-    </View>
-    <Text style={styles.listHeader}>Already have some knowledge?</Text>
-    <Text style={styles.listSubHeader}>
-      {" "}
-      Check out the tabs to learn more about:{" "}
-    </Text>
-    <View style={styles.optionswrapper}>
-      <TouchableOpacity onPress={() => navigation.navigate("Navigation")}>
-        <Unorderedlist style={styles.bulletItem}>
-          <Text style={styles.bulletText}>Navigation</Text>
-        </Unorderedlist>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("State Management")}>
-        <Unorderedlist style={styles.bulletItem}>
-          <Text style={styles.bulletText}>State Management</Text>
-        </Unorderedlist>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Native Device Features")}
-      >
-        <Unorderedlist style={styles.bulletItem}>
-          <Text style={styles.bulletText}>Using Native Device Features</Text>
-        </Unorderedlist>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Debugging")}>
-        <Unorderedlist style={styles.bulletItem}>
-          <Text style={styles.bulletText}>Debugging</Text>
-        </Unorderedlist>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Publishing")}>
-        <Unorderedlist style={styles.bulletItem}>
-          <Text style={styles.bulletText}>and Publishing Your App</Text>
-        </Unorderedlist>
-      </TouchableOpacity>
-    </View>
-    <TouchableOpacity
-      style={styles.nextButton}
-      onPress={() => navigation.navigate("What is React Native")}
-    >
-      <Text style={styles.nextButtonText}>Next</Text>
-      <Ionicons name="arrow-forward-outline" size={24} color="white" />
-    </TouchableOpacity>
+    </ScrollView>
   </View>
 );
 
@@ -81,7 +96,6 @@ const styles = StyleSheet.create({
   screenwrapper: {
     flex: 1,
     marginHorizontal: 20,
-    marginVertical: 20,
   },
   title: {
     fontSize: 30,
